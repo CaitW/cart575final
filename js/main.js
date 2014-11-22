ef4b0d7ef03c5ff112c649676402a5a07e832c9d
CaitW <wolters.caitlin@gmail.com>
2014-11-22 12:35:02 -0600

    getting leaflet initialized

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+$(document).ready(function () {
+
+
+	var map = L.map('map').setView([43.4149, -89.7262], 14);
+
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+
+
+});
