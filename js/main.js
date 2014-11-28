3d1245a1ccdcecbce4158a6ca5ff325f5d636b5b
CaitW <wolters.caitlin@gmail.com>
2014-11-28 16:35:50 -0600

    NPS tiles are now up and working!

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(document).ready(function () {
 
 	var map = L.map('map').setView([43.4180,-89.7297], 13);
 
-	L.tileLayer('data/tileserver/tiles/{z}/{x}/{y}.png').addTo(map);
+	L.tileLayer('https://a.tiles.mapbox.com/v4/nps.2yxv8n84,nps.jhd2e8lb/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6Ik5yOFVUR2sifQ.lcpvx7UEgHGoeObibjqMBw').addTo(map);
 
 
 });
+
+//https://a.tiles.mapbox.com/v4/nps.2yxv8n84,nps.jhd2e8lb/5/6/12.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6IkdfeS1OY1UifQ.K8Qn5ojTw4RV1GwBlsci-Q
