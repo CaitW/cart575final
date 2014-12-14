a75172f5c45a2f4b93d418a195a5f0616d7dfd1a
CaitW <wolters.caitlin@gmail.com>
2014-12-14 14:02:40 -0600

    Nav bar in place (not working)

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ var infoBarSlideNumber = 0;
 $(document).ready(function () {
 
 	// append the contents of our first slide
-	$("#barHeader").html(slides[0].title);
-	$("#barBody").html(slides[0].body);
+	//$("#barHeader").html(slides[0].title);
+	//$("#barBody").html(slides[0].body);
 
 	// default bounding box of the initial view of the map
 	var defaultBbox = [[43.43,-89.71],[43.405,-89.75]];
@@ -191,7 +191,7 @@ $(document).ready(function () {
 			$("#barHeader, #barBody").effect("slide", {direction: "right", mode:"show"}, 200);
 		});
 
-		$(document).trigger("slideChange", [slides[infoBarSlideNumber].id]);
+		//$(document).trigger("slideChange", [slides[infoBarSlideNumber].id]);
 	});
 
 	$(document).on("click", "#leftScroll i", function () {
@@ -213,7 +213,7 @@ $(document).ready(function () {
 			$("#barHeader, #barBody").effect("slide", {direction: "left", mode:"show"}, 200);
 		});
 
-		$(document).trigger("slideChange", [slides[infoBarSlideNumber].id]);
+		//$(document).trigger("slideChange", [slides[infoBarSlideNumber].id]);
 	});
 
 	//////////////////////
@@ -223,251 +223,257 @@ $(document).ready(function () {
 		////////////////////////////
 		// Basemaps / Tile Layers // 
 		////////////////////////////
+		// {
+			basemaps = {
+				"hillshade": L.tileLayer('https://a.tiles.mapbox.com/v4/nps.a6be40f0/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6Ik5yOFVUR2sifQ.lcpvx7UEgHGoeObibjqMBw'),
+				"alternateHillshade": L.esri.tiledMapLayer('http://basemap.nationalmap.gov/arcgis/rest/services/USGSShadedReliefOnly/MapServer').setOpacity(0.3).addTo(map),
+				//"bluff labels": L.tileLayer('data/custom-tiles/bluffLabels/{z}/{x}/{y}.png').addTo(map),
+				"parks boundaries": L.tileLayer('data/custom-tiles/boundaries/{z}/{x}/{y}.png').setOpacity(0.4).addTo(map),
+				"boundaries and natural": L.tileLayer('data/custom-tiles/naturalAreas/{z}/{x}/{y}.png').setOpacity(0.4), //.addTo(map),
+				"water": L.tileLayer('https://a.tiles.mapbox.com/v4/nps.a706dc69/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6Ik5yOFVUR2sifQ.lcpvx7UEgHGoeObibjqMBw').addTo(map),
+				"roads": L.tileLayer('https://a.tiles.mapbox.com/v4/nps.8eb491cc/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6Ik5yOFVUR2sifQ.lcpvx7UEgHGoeObibjqMBw').setOpacity(0.8).addTo(map),
+				"trails": L.tileLayer('data/custom-tiles/trails/{z}/{x}/{y}.png').setOpacity(0.5).addTo(map),
+				"labels": L.tileLayer('https://a.tiles.mapbox.com/v4/nps.5dfeaf68/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6Ik5yOFVUR2sifQ.lcpvx7UEgHGoeObibjqMBw').addTo(map)
+			};
 
-		basemaps = {
-			"hillshade": L.tileLayer('https://a.tiles.mapbox.com/v4/nps.a6be40f0/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6Ik5yOFVUR2sifQ.lcpvx7UEgHGoeObibjqMBw'),
-			"alternateHillshade": L.esri.tiledMapLayer('http://basemap.nationalmap.gov/arcgis/rest/services/USGSShadedReliefOnly/MapServer').setOpacity(0.3).addTo(map),
-			//"bluff labels": L.tileLayer('data/custom-tiles/bluffLabels/{z}/{x}/{y}.png').addTo(map),
-			"parks boundaries": L.tileLayer('data/custom-tiles/boundaries/{z}/{x}/{y}.png').setOpacity(0.4).addTo(map),
-			"boundaries and natural": L.tileLayer('data/custom-tiles/naturalAreas/{z}/{x}/{y}.png').setOpacity(0.4), //.addTo(map),
-			"water": L.tileLayer('https://a.tiles.mapbox.com/v4/nps.a706dc69/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6Ik5yOFVUR2sifQ.lcpvx7UEgHGoeObibjqMBw').addTo(map),
-			"roads": L.tileLayer('https://a.tiles.mapbox.com/v4/nps.8eb491cc/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6Ik5yOFVUR2sifQ.lcpvx7UEgHGoeObibjqMBw').setOpacity(0.8).addTo(map),
-			"trails": L.tileLayer('data/custom-tiles/trails/{z}/{x}/{y}.png').setOpacity(0.5).addTo(map),
-			"labels": L.tileLayer('https://a.tiles.mapbox.com/v4/nps.5dfeaf68/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6Ik5yOFVUR2sifQ.lcpvx7UEgHGoeObibjqMBw').addTo(map)
-		};
-
-		// populated below
-		layers = {};
+			// populated below
+			layers = {};
+		// }
 
 		////////////////////////
 		// Overlays: Polygons //
 		////////////////////////
+		// {
 
-		$.getJSON("data/buildings.json", function (data) {
-			var style = {
-				"color": "#7e7e7e",
-			    "weight": 0,
-			    "fillOpacity": 0.5
-			};
-			layers["Buildings"] = L.geoJson(data, {
-				style: style,
-				onEachFeature: function (feature, layer) {
-					layer.setStyle({clickable: false})
-				}
-			}).addTo(map);
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Buildings"], "Buildings", true, "img/buildings.svg"]);
-		});
-
-		$.getJSON("data/campsiteOutlines.json", function (data) {
-			var style = {
-				"color": "#333333",
-			    "weight": 0,
-			    "opacity": 0.3
-			};
-			layers["Campsites"] = L.geoJson(data, { 
-				style: style,
-				onEachFeature: function (feature, layer) {
-					layer.setStyle({clickable: false})
-				}
+			$.getJSON("data/buildings.json", function (data) {
+				var style = {
+					"color": "#7e7e7e",
+				    "weight": 0,
+				    "fillOpacity": 0.5
+				};
+				layers["Buildings"] = L.geoJson(data, {
+					style: style,
+					onEachFeature: function (feature, layer) {
+						layer.setStyle({clickable: false})
+					}
+				}).addTo(map);
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Buildings"], "Buildings", true, "img/buildings.svg"]);
 			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Campsites"], "Campsites", false]);
-		});
 
+			$.getJSON("data/campsiteOutlines.json", function (data) {
+				var style = {
+					"color": "#333333",
+				    "weight": 0,
+				    "opacity": 0.3
+				};
+				layers["Campsites"] = L.geoJson(data, { 
+					style: style,
+					onEachFeature: function (feature, layer) {
+						layer.setStyle({clickable: false})
+					}
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Campsites"], "Campsites", false]);
+			});
+		// }
 
 		//////////////////////
 		// Overlays: Points //
 		//////////////////////
+		// {
 
-		$.getJSON("data/boatLaunch.json", function (data) {
-			layers["Boat Launches"] = L.geoJson(data, {
-				pointToLayer: function (feature, latlng) {
-					var marker = L.divIcon({
-						className: "boatLaunches",
-						html: "<img src='img/boat-launch-NPS.svg'>" // I do think these should be static sizes---the vh makes them too responsive + small on other screens. can edit in CSS>
-					});
-					return L.marker(latlng, {
-						icon: marker,
-						clickable: false
-					})
-				}
+			$.getJSON("data/boatLaunch.json", function (data) {
+				layers["Boat Launches"] = L.geoJson(data, {
+					pointToLayer: function (feature, latlng) {
+						var marker = L.divIcon({
+							className: "boatLaunches",
+							html: "<img src='img/boat-launch-NPS.svg'>" // I do think these should be static sizes---the vh makes them too responsive + small on other screens. can edit in CSS>
+						});
+						return L.marker(latlng, {
+							icon: marker,
+							clickable: false
+						})
+					}
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Boat Launches"], "Boat Launches", true, 'img/boat-launch-NPS.svg']);
 			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Boat Launches"], "Boat Launches", true, 'img/boat-launch-NPS.svg']);
-		});
 
-		$.getJSON("data/historicalPoints.json", function (data) {
-			layers["Historical Points"] = L.geoJson(data, {
-				pointToLayer: function (feature, latlng) {
-					var marker = L.divIcon({
-						className: "historicalPoints",
-						//html: "<img src='img/historical-points.svg'>"
-					});
-					return L.marker(latlng, {
-						icon: marker
-					})
-				},
-				onEachFeature: function(feature, layer) {
-					layer.bindPopup("<center><h3>" + feature.properties.Name + "</h3><br>" + feature.properties.Description + "</center>");
-				}
+			$.getJSON("data/historicalPoints.json", function (data) {
+				layers["Historical Points"] = L.geoJson(data, {
+					pointToLayer: function (feature, latlng) {
+						var marker = L.divIcon({
+							className: "historicalPoints",
+							//html: "<img src='img/historical-points.svg'>"
+						});
+						return L.marker(latlng, {
+							icon: marker
+						})
+					},
+					onEachFeature: function(feature, layer) {
+						layer.bindPopup("<center><h3>" + feature.properties.Name + "</h3><br>" + feature.properties.Description + "</center>");
+					}
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Historical Points"], "Historical Points", true, 'img/old/historical-points.svg']);
 			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Historical Points"], "Historical Points", true, 'img/old/historical-points.svg']);
-		});
 
-		$.getJSON("data/mounds.json", function (data) {
-			layers["Native American Mounds"] = L.geoJson(data, {
-				pointToLayer: function (feature, latlng) {
-					var marker = L.divIcon({
-						className: "mounds",
-						html: "<img src='img/mounds-NPSish.svg'>"
-					});
-					return L.marker(latlng, {
-						icon: marker
-					})
-				},
-				onEachFeature: function(feature, layer) {
-					layer.bindPopup("<center><h3>" + feature.properties.Name + "</h3><br>" + feature.properties.Description + "</center>");
-				}
+			$.getJSON("data/mounds.json", function (data) {
+				layers["Native American Mounds"] = L.geoJson(data, {
+					pointToLayer: function (feature, latlng) {
+						var marker = L.divIcon({
+							className: "mounds",
+							html: "<img src='img/mounds-NPSish.svg'>"
+						});
+						return L.marker(latlng, {
+							icon: marker
+						})
+					},
+					onEachFeature: function(feature, layer) {
+						layer.bindPopup("<center><h3>" + feature.properties.Name + "</h3><br>" + feature.properties.Description + "</center>");
+					}
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Native American Mounds"], "Native American Mounds", true, 'img/mounds-NPSish.svg']);
 			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Native American Mounds"], "Native American Mounds", true, 'img/mounds-NPSish.svg']);
-		});
 
-		$.getJSON("data/pointsOfInterest.json", function (data) {
-			layers["Points of Interest"] = L.geoJson(data, {
-				pointToLayer: function (feature, latlng) {
-					var marker = L.divIcon({
-						className: "pointsOfInterest",
-						html: "<img src='img/geology-NPS.svg'>"
-					});
-					return L.marker(latlng, {
-						icon: marker
-					})
-				},
-				onEachFeature: function(feature, layer) {
-					layer.bindPopup("<center><h3>" + feature.properties.Name + "</h3></center>");
-				}
+			$.getJSON("data/pointsOfInterest.json", function (data) {
+				layers["Points of Interest"] = L.geoJson(data, {
+					pointToLayer: function (feature, latlng) {
+						var marker = L.divIcon({
+							className: "pointsOfInterest",
+							html: "<img src='img/geology-NPS.svg'>"
+						});
+						return L.marker(latlng, {
+							icon: marker
+						})
+					},
+					onEachFeature: function(feature, layer) {
+						layer.bindPopup("<center><h3>" + feature.properties.Name + "</h3></center>");
+					}
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Points of Interest"], "Points of Interest", true, 'img/geology-NPS.svg']);
 			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Points of Interest"], "Points of Interest", true, 'img/geology-NPS.svg']);
-		});
 
-		$.getJSON("data/campsiteCenterpoints.json", function (data) {
-			layers["Campsite Centerpoints"] = L.geoJson(data, {
-				pointToLayer: function (feature, latlng) {
-					var marker = L.divIcon({
-						className: "campsiteCenterpoints",
-						html: "<img src='img/campsites-NPS.svg'>"
-					});
-					return L.marker(latlng, {
-						icon: marker
-					})
-				},
-				onEachFeature: function(feature, layer) {
-					layer.bindPopup("<center><h3>" + feature.properties.Name + "</h3></center>");
-				}
+			$.getJSON("data/campsiteCenterpoints.json", function (data) {
+				layers["Campsite Centerpoints"] = L.geoJson(data, {
+					pointToLayer: function (feature, latlng) {
+						var marker = L.divIcon({
+							className: "campsiteCenterpoints",
+							html: "<img src='img/campsites-NPS.svg'>"
+						});
+						return L.marker(latlng, {
+							icon: marker
+						})
+					},
+					onEachFeature: function(feature, layer) {
+						layer.bindPopup("<center><h3>" + feature.properties.Name + "</h3></center>");
+					}
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Campsite Centerpoints"], "Campsite Centerpoints", true, 'img/campsites-NPS.svg']);
 			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Campsite Centerpoints"], "Campsite Centerpoints", true, 'img/campsites-NPS.svg']);
-		});
 
-		$.getJSON("data/shelters.json", function (data) {
-			layers["Shelters"]= L.geoJson(data, {
-				pointToLayer: function (feature, latlng) {
-					var marker = L.divIcon({
-						className: "shelters",
-						html: "<img src='img/shelters-1-NPS.svg'>"
-					});
-					return L.marker(latlng, {
-						icon: marker, 
-						clickable: false
-					})
-				}
+			$.getJSON("data/shelters.json", function (data) {
+				layers["Shelters"]= L.geoJson(data, {
+					pointToLayer: function (feature, latlng) {
+						var marker = L.divIcon({
+							className: "shelters",
+							html: "<img src='img/shelters-1-NPS.svg'>"
+						});
+						return L.marker(latlng, {
+							icon: marker, 
+							clickable: false
+						})
+					}
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Shelters"], "Shelters", true, 'img/shelters-1-NPS.svg']);
 			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Shelters"], "Shelters", true, 'img/shelters-1-NPS.svg']);
-		});
 
-		$.getJSON("data/parkingLots.json", function (data) {
-			layers["Parking Lots"]= L.geoJson(data, {
-				pointToLayer: function (feature, latlng) {
-					var marker = L.divIcon({
-						className: "parkingLots",
-						//html: "<img src='img/'>"
-					});
-					return L.marker(latlng, {
-						icon: marker
-					})
-				}
+			$.getJSON("data/parkingLots.json", function (data) {
+				layers["Parking Lots"]= L.geoJson(data, {
+					pointToLayer: function (feature, latlng) {
+						var marker = L.divIcon({
+							className: "parkingLots",
+							//html: "<img src='img/'>"
+						});
+						return L.marker(latlng, {
+							icon: marker
+						})
+					}
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Parking Lots"], "Parking Lots", true]);
 			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Parking Lots"], "Parking Lots", true]);
-		});
 
-		$.getJSON("data/restrooms.json", function (data) {
-			layers["Restrooms"]= L.geoJson(data, {
-				pointToLayer: function (feature, latlng) {
-					var marker = L.divIcon({
-						className: "restrooms",
-						html: "<img src='img/restrooms-NPS.svg'>"
-					});
-					return L.marker(latlng, {
-						icon: marker, 
-						clickable: false
-					})
-				}
+			$.getJSON("data/restrooms.json", function (data) {
+				layers["Restrooms"]= L.geoJson(data, {
+					pointToLayer: function (feature, latlng) {
+						var marker = L.divIcon({
+							className: "restrooms",
+							html: "<img src='img/restrooms-NPS.svg'>"
+						});
+						return L.marker(latlng, {
+							icon: marker, 
+							clickable: false
+						})
+					}
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Restrooms"], "Restrooms", true, 'img/restrooms-NPS.svg']);
 			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Restrooms"], "Restrooms", true, 'img/restrooms-NPS.svg']);
-		});
+		// }
 
 		/////////////////////////
 		// Overlays: Polylines //
 		/////////////////////////
+		// {
 
-		// these are actually not visible, but just provide clicking functionality
-		$.getJSON("data/trails.json", function (data) {
+			// these are actually not visible, but just provide clicking functionality
+			$.getJSON("data/trails.json", function (data) {
 
-			style = {
-				"color": "#856363",
-			    "weight": 25,
-			    "opacity": 0.01
-			};
-			layers["Trails"] = L.geoJson(data, {
-				className: "trails",
-				style: style,
-				onEachFeature: function (feature, layer) {
-					if(feature.properties.name != null)
-					{
-						layer.bindPopup("<center><h3>" + feature.properties.name + "</center></h3>");
+				style = {
+					"color": "#856363",
+				    "weight": 25,
+				    "opacity": 0.01
+				};
+				layers["Trails"] = L.geoJson(data, {
+					className: "trails",
+					style: style,
+					onEachFeature: function (feature, layer) {
+						if(feature.properties.name != null)
+						{
+							layer.bindPopup("<center><h3>" + feature.properties.name + "</center></h3>");
+						}
 					}
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Trails"], "Trails", true, "img/trail-curvy.svg"]);
+			});
+
+			$.getJSON("data/bathymetry.json", function (data) {
+
+				var style = {
+					"color": "#90CAF9",
+				    "weight": 0.9,
+				    "opacity": 0.65
 				}
+
+				layers["Bathymetry"] = L.geoJson(data, {
+					className: "bathymetry",
+					style: style
+				});
+			}).complete(function() {
+				$(document).trigger("layeradd", [layers["Bathymetry"], "Bathymetry", false]);
 			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Trails"], "Trails", true, "img/trail-curvy.svg"]);
-		});
-
-		$.getJSON("data/bathymetry.json", function (data) {
-
-			var style = {
-				"color": "#90CAF9",
-			    "weight": 0.9,
-			    "opacity": 0.65
-			}
-
-			layers["Bathymetry"] = L.geoJson(data, {
-				className: "bathymetry",
-				style: style
-			});
-		}).complete(function() {
-			$(document).trigger("layeradd", [layers["Bathymetry"], "Bathymetry", false]);
-		});
+		// }
 
 
 });
 
 $(window).load(function() {
-	$(document).trigger("slideChange", "welcome");
+	
 });
